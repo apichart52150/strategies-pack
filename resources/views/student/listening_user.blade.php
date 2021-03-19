3d7589a4d90b319bec37e11b63fa6f05bca20c3f
Apichart Partoommart <apichart.p@newcambridge.net>
2021-03-19 14:09:30 +0700

    commit

--- resources/views/student/listening_user.blade.php
+++ resources/views/student/listening_user.blade.php
@@ -4,23 +4,23 @@
 @section('content')
 
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-<link rel="stylesheet" href="https://cdn.plyr.io/3.6.3/plyr.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.6.3/plyr.css" />
 
 
     <div class="content">
-                    
+                        
         <div class="container-fluid">
                                             
             <div class="row">
                     <div class="col-12">
                         <div class="page-title-box">
-                            <h4 class="page-title">Listening</h4>
+                            <h4 class="page-title">Reading</h4>
                         </div>
                     </div>
             </div> 
-            
+
             <!-- <center> <h1 style="color:#616161">Coming Soon . .</h1> -->
             <center>
             <div class="row">  
@@ -29,7 +29,8 @@
 
                     @if($user->status == "active")
 
-                        @if($user->topic == "listening")
+                        @if($user->topic == "reading")
+
                             <div class="col-lg-6"> <!--card 1-->
                                 <div class="card-box ribbon-box">
                                     <div class="ribbon ribbon-primary float-left"> <i class="remixicon-eye-fill mr-1"></i>{{$user->title}}</div>
@@ -51,14 +52,13 @@
                                                 <button class="btn btn-dark disabled mt-2">No sample answer</button>
                                             
                                             @else
-    
+
                                                 <a download href ="{{ asset('storage/app/public/file/'.$user->file_path) }}" class="btn btn-icon waves-effect waves-light btn-primary mt-2"> <i class="fe-download"></i> Sample answer PDF </a>
                                         
                                             @endif
                                         </h4>
-
-                                        </div>
-                                    </div>    
+                                            
+                                    </div>
 
                                 </div>
 
@@ -67,15 +67,13 @@
                         @endif
 
                     @endif
-                
+
                 @endforeach
 
-            </div> 
+            </div> <!--End Row-->    
 
-     
         </div> <!--end container-fluid-->
-        
-               
+                        
     </div> <!--end content --> 
 
 
--- resources/views/student/reading_user.blade.php
+++ resources/views/student/reading_user.blade.php
@@ -3,9 +3,9 @@
 
 @section('content')
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-<link rel="stylesheet" href="https://cdn.plyr.io/3.6.3/plyr.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.6.3/plyr.css" />
 
     <div class="content">
                     
