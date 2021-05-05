64fa7b97810709a7cf9cf49a13c0a1191b5a9840
Apichart Partoommart <apichart.p@newcambridge.net>
2021-05-05 13:30:44 +0700

    Update DataViewController.php

--- app/Http/Controllers/DataViewController.php
+++ app/Http/Controllers/DataViewController.php
@@ -11,36 +11,30 @@ use Auth;
 class DataViewController extends Controller
 {
     public function strategies_home(){
-        $std_id = auth('student')->user()->std_id;
         return view('student.strategies_home');
     }
 
     public function introduction(){
-    	$std_id = auth('student')->user()->std_id;
         $users = DB::select('select * from video');
         return view('student.introduction',['users'=>$users]);
     }
 
     public function listening_user(){
-    	$std_id = auth('student')->user()->std_id;
         $users = DB::select('select * from video');
         return view('student.listening_user',['users'=>$users]);
     }
 
     public function reading_user(){
-        $std_id = auth('student')->user()->std_id;
         $users = DB::select('select * from video');
         return view('student.reading_user',['users'=>$users]);
     }
 
     public function writing_user(){
-        $std_id = auth('student')->user()->std_id;
         $users = DB::select('select * from video');
         return view('student.writing_user',['users'=>$users]);
     }
 
     public function speaking_user(){
-        $std_id = auth('student')->user()->std_id;
         $users = DB::select('select * from video');
         return view('student.speaking_user',['users'=>$users]);
     }
